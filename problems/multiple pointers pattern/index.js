e67c344ceceac9e8a88559435174a82517d99336
unknown <pingo15102002@gmail.com>
2023-08-09 20:43:53 +0200

    Problems end

--- problems/multiple pointers pattern/index.js
+++ problems/multiple pointers pattern/index.js
@@ -28,7 +28,7 @@ function sumZero(arr) {
 }
 
 var arr = [-4, -3, -2, -1, 0, 1, 2, 3, 10];
-console.log(sumZero(arr));
+// console.log(sumZero(arr));
 
 // Count unique values
 /* 
@@ -54,7 +54,97 @@ function countUniqueValues(arr) {
 	return i + 1;
 }
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
-console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
-console.log(countUniqueValues([]));
-console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+// console.log(countUniqueValues([]));
+// console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+
+// Problem #1 - averagePair
+/*
+Write a function called averagePair. 
+Given a sorted array of integers and a target average, 
+determine if there is a pair of values in the array where the average of the pair equals the target average. 
+There may be more than one pair that matches the average target.
+
+averagePair([1,2,3],2.5) // true
+averagePair([1,3,3,5,6,7,10,12,19],8) // true
+averagePair([-1,0,3,4,5,6], 4.1) // false
+averagePair([],4) // false
+*/
+
+//  i     j --> average (1+3) / 2 = 2 --> avr < target i++ else j--
+//     i  j --> average (2+3) / 2 = 2.5 return true
+// [1, 2, 3]
+
+function averagePair(arr, target) {
+	var average = 0;
+	var left = 0;
+	var right = arr.length - 1;
+
+	// loop thru while left < right
+	while (left < right) {
+		average = (arr[left] + arr[right]) / 2;
+		console.log(average);
+
+		if (average === target) return true;
+
+		if (average < target) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return false;
+}
+
+// console.log(averagePair([1, 2, 3], 2.5));
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+// console.log(averagePair([], 4));
+
+// Problem #2 - isSubsequence
+/*
+Write a function called isSubsequence which takes in two strings 
+and checks whether the characters in the first string form a subsequence of the characters in the second string. 
+In other words, 
+the function should check whether the characters in the first string appear somewhere in the second string, 
+without their order changing.
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+*/
+
+// i = 0; j = 0
+// i      j   -> i=j i++ j++
+//  i      j  -> i!=j j++
+//  i       j  -> i=j i++ j++
+// sing - sting
+
+// if i = str1.length return true
+
+function isSubsequence(str1, str2) {
+	var subStrCount = 0;
+
+	// if there is no str1 return true
+	if (!str1) return true;
+
+	// Loop thru second string
+	for (var j = 0; j < str2.length; j++) {
+		// check if str1[subStrCount] == str2[j] if is increment subStrCount and j
+		if (str1[subStrCount] === str2[j]) {
+			subStrCount++;
+		}
+		// Check if subStrCount is equal to str1 length
+		if (subStrCount === str1.length) return true;
+	}
+	// return false if no substring was found
+	return false;
+}
+
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'acb')); // false (order matters)
--- problems/sliding windows pattern/index.js
+++ problems/sliding windows pattern/index.js
@@ -97,4 +97,117 @@ function maxSubarraySum2(arr, num) {
 	return maxSum;
 }
 
-console.log(maxSubarraySum2([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// console.log(maxSubarraySum2([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+// Problem #1 - maxSubarraySum
+
+/*
+Given an array of integers and a number, write a function called maxSubarraySum, 
+which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+Note that a subarray must consist of consecutive elements from the original array. 
+In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+maxSubarraySum([100,200,300,400], 2) // 700
+maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)  // 39 
+maxSubarraySum([-3,4,0,-2,6,-1], 2) // 5
+maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
+maxSubarraySum([2,3], 3) // null
+*/
+
+//  i    i
+
+// next loop
+//           i  -> 300 + 300 - 200 = 400
+//               i -> 400 + 400 - 300 =
+// [100,200,300,400], 2
+function maxSubarraySum(arr, n) {
+	// Initialize maxSum and tempSum
+	var maxSum = 0;
+	var tempSum = 0;
+
+	// If array length is less than n return null
+	if (arr.length < n) return null;
+
+	// Add first n numbers to max sum
+	for (var i = 0; i < n; i++) {
+		maxSum += arr[i];
+	}
+
+	// Set temp sum as max sum
+	tempSum = maxSum;
+
+	// loop thru from n to end of array
+	for (var i = n; i < arr.length; i++) {
+		// add new number to temp sum and subtract previous number
+		// console.log(arr[i]);
+		// console.log(arr[i - n]);
+		// console.log('break');
+		tempSum = tempSum - arr[i - n] + arr[i];
+
+		// if maxSum is less than tempSum set maxSum to tempSum
+		if (maxSum < tempSum) {
+			maxSum = tempSum;
+		}
+	}
+
+	// return maxSum
+	return maxSum;
+}
+
+// console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+// console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+// console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+// console.log(maxSubarraySum([2, 3], 3)); // null
+
+// Problem #2 - minSubArrayLen
+
+/*
+Write a function called minSubArrayLen which accepts two parameters 
+- an array of positive integers and a positive integer
+
+This function should return 
+the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. 
+If there isn't one, return 0 instead.
+
+Examples:
+
+minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> because [4,3] is the smallest subarray
+minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
+minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
+minSubArrayLen([1,4,16,22,5,7,8,9,10],39) // 3
+minSubArrayLen([1,4,16,22,5,7,8,9,10],55) // 5
+minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
+minSubArrayLen([1,4,16,22,5,7,8,9,10],95) // 0
+
+*/
+
+function minSubArrayLen(nums, sum) {
+	let total = 0;
+	let start = 0;
+	let end = 0;
+	let minLen = Infinity;
+
+	while (start < nums.length) {
+		// if current window doesn't add up to the given sum then
+		// move the window to right
+		if (total < sum && end < nums.length) {
+			total += nums[end];
+			end++;
+		}
+		// if current window adds up to at least the sum given then
+		// we can shrink the window
+		else if (total >= sum) {
+			minLen = Math.min(minLen, end - start);
+			total -= nums[start];
+			start++;
+		}
+		// current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+		else {
+			break;
+		}
+	}
+
+	return minLen === Infinity ? 0 : minLen;
+}
